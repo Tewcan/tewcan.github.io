faace7b8a08391534c690a84a4b274ba66414f13
Williamtew <william@nevada.unr.edu>
2016-02-15 20:50:02 -0800

    Uploaded my site
    Because reasons!!

---
+++ js/rps_js.js
@@ -0,0 +1,141 @@
+﻿var pScore = 0
+var cScore = 0
+var tScore = 0
+var total = 0
+var pWinRate = 0
+var cWinRate = 0
+var tRate = 0
+var displayStatus = 1
+
+function game(x) {
+    var player = x
+    var computer = Math.floor(Math.random() * 3) + 1
+    var pResult
+    var cResult
+    total += 1
+    document.getElementById("total").innerHTML = total
+
+    switch (player) {
+        case 1:
+            pResult = "Rock"
+            break;
+        case 2:
+            pResult = "Paper"
+            break;
+        case 3:
+            pResult = "Scissors"
+            break;
+    }
+    switch (computer) {
+        case 1:
+            cResult = "Rock"
+            break;
+        case 2:
+            cResult = "Paper"
+            break;
+        case 3:
+            cResult = "Scissors"
+            break;
+    }
+
+    document.getElementById("player").innerHTML = pResult
+    document.getElementById("computer").innerHTML = cResult
+    
+    var node = document.createElement("LI");                 
+    var textnode = document.createTextNode("You picked " + pResult + ". I picked " + cResult);         
+    node.appendChild(textnode);                              
+    document.getElementById("feedlist").insertBefore(node, document.getElementById("feedlist").childNodes[0]);
+    
+
+    if (player === computer) {
+        document.getElementById("status").innerHTML = "Tie"
+        tScore += 1
+        document.getElementById("tScore").innerHTML = tScore
+        winRates()
+        return
+    }
+
+    if (player === 1 && computer === 3) {
+        document.getElementById("status").innerHTML = "You win"
+        pScore += 1
+        document.getElementById("pScore").innerHTML = pScore
+        winRates()
+        return
+    }
+
+    if (player === 2 && computer === 1) {
+        document.getElementById("status").innerHTML = "You win"
+        pScore += 1
+        document.getElementById("pScore").innerHTML = pScore
+        winRates()
+        return 
+    }
+
+    if (player === 3 && computer === 2) {
+        document.getElementById("status").innerHTML = "You win"
+        pScore += 1
+        document.getElementById("pScore").innerHTML = pScore
+        winRates()
+        return 
+    }
+
+    document.getElementById("status").innerHTML = "I win"
+    cScore += 1
+    document.getElementById("cScore").innerHTML = cScore
+    winRates()
+    return 
+}
+
+function randomGame(x) {
+    var a = 0
+    var i = 0
+    while (i < x) {
+        var a = Math.floor(Math.random() * 3) + 1
+        game(a)
+        i += 1
+    }
+}
+
+function toggleDisplay() {
+    if (displayStatus === 1) {
+        document.getElementById("feed").style = "display: block;"
+        displayStatus = 0
+        return
+    }
+    if (displayStatus === 0) {
+        document.getElementById("feed").style = "display: none;"
+        displayStatus = 1
+        return
+    }
+}
+
+function clearAll() {
+    pScore = 0
+    cScore = 0
+    tScore = 0
+    pWinRate = 0
+    cWinRate = 0
+    tRate = 0
+    total = 0
+    document.getElementById("pScore").innerHTML = 0
+    document.getElementById("cScore").innerHTML = 0
+    document.getElementById("tScore").innerHTML = 0
+    document.getElementById("feedlist").innerHTML = ""
+    document.getElementById("player").innerHTML = ""
+    document.getElementById("computer").innerHTML = ""
+    document.getElementById("status").innerHTML = ""
+    document.getElementById("pWinRate").innerHTML = 0
+    document.getElementById("cWinRate").innerHTML = 0
+    document.getElementById("tRate").innerHTML = 0
+    document.getElementById("total").innerHTML = 0
+
+}
+
+function winRates() {
+    pWinRate = pScore / total * 100
+    document.getElementById("pWinRate").innerHTML = pWinRate.toFixed(2) + "%"
+    cWinRate = cScore / total * 100
+    document.getElementById("cWinRate").innerHTML = cWinRate.toFixed(2) + "%"
+    tRate = tScore / total * 100
+    document.getElementById("tRate").innerHTML = tRate.toFixed(2) + "%"
+}
---
+++ js/tic_js.js
@@ -0,0 +1,154 @@
+﻿
+var turn = 0
+var gameStatus = 0
+var blueWins = 0
+var redWins = 0
+var gameBoard = [0,0,0,0,0,0,0,0,0,0];
+
+function tester(x, y) {
+    if (gameStatus === 0) {
+        if (gameBoard[y] === 0) {
+            if (turn === 0) {
+                document.getElementById(x).style = "background-color: blue;"
+                gameBoard[y] = 1
+                turn = 1
+                document.getElementById("status").innerHTML = "Red's Turn"
+            }
+            else if (turn === 1) {
+                document.getElementById(x).style = "background-color: red;"
+                gameBoard[y] = 2
+                turn = 0
+                document.getElementById("status").innerHTML = "Blue's Turn"
+            }
+            drawCheck()
+            blueCheck()
+            redCheck()            
+            updateScore()
+        }        
+    }
+}
+
+function blueCheck() {
+    if (gameBoard[1] === 1 && gameBoard[2] === 1 && gameBoard[3] === 1) {
+        document.getElementById("status").innerHTML = "Blue Wins!"
+        gameStatus = 1
+        blueWins += 1
+    }
+    else if (gameBoard[4] === 1 && gameBoard[5] === 1 && gameBoard[6] === 1) {
+        document.getElementById("status").innerHTML = "Blue Wins!"
+        gameStatus = 1
+        blueWins += 1
+    }
+    else if (gameBoard[7] === 1 && gameBoard[8] === 1 && gameBoard[9] === 1) {
+        document.getElementById("status").innerHTML = "Blue Wins!"
+        gameStatus = 1
+        blueWins += 1
+    }
+    else if (gameBoard[1] === 1 && gameBoard[4] === 1 && gameBoard[7] === 1) {
+        document.getElementById("status").innerHTML = "Blue Wins!"
+        gameStatus = 1
+        blueWins += 1
+    }
+    else if (gameBoard[2] === 1 && gameBoard[5] === 1 && gameBoard[8] === 1) {
+        document.getElementById("status").innerHTML = "Blue Wins!"
+        gameStatus = 1
+        blueWins += 1
+    }
+    else if (gameBoard[3] === 1 && gameBoard[6] === 1 && gameBoard[9] === 1) {
+        document.getElementById("status").innerHTML = "Blue Wins!"
+        gameStatus = 1
+        blueWins += 1
+    }
+    else if (gameBoard[1] === 1 && gameBoard[5] === 1 && gameBoard[9] === 1) {
+        document.getElementById("status").innerHTML = "Blue Wins!"
+        gameStatus = 1
+        blueWins += 1
+    }
+    else if (gameBoard[3] === 1 && gameBoard[5] === 1 && gameBoard[7] === 1) {
+        document.getElementById("status").innerHTML = "Blue Wins!"
+        gameStatus = 1
+        blueWins += 1
+    }
+}
+
+function redCheck() {
+    if (gameBoard[1] === 2 && gameBoard[2] === 2 && gameBoard[3] === 2) {
+        document.getElementById("status").innerHTML = "Red Wins!"
+        gameStatus = 1
+        redWins += 1
+    }
+    else if (gameBoard[4] === 2 && gameBoard[5] === 2 && gameBoard[6] === 2) {
+        document.getElementById("status").innerHTML = "Red Wins!"
+        gameStatus = 1
+        redWins += 1
+    }
+    else if (gameBoard[7] === 2 && gameBoard[8] === 2 && gameBoard[9] === 2) {
+        document.getElementById("status").innerHTML = "Red Wins!"
+        gameStatus = 1
+        redWins += 1
+    }
+    else if (gameBoard[1] === 2 && gameBoard[4] === 2 && gameBoard[7] === 2) {
+        document.getElementById("status").innerHTML = "Red Wins!"
+        gameStatus = 1
+        redWins += 1
+    }
+    else if (gameBoard[2] === 2 && gameBoard[5] === 2 && gameBoard[8] === 2) {
+        document.getElementById("status").innerHTML = "Red Wins!"
+        gameStatus = 1
+        redWins += 1
+    }
+    else if (gameBoard[3] === 2 && gameBoard[6] === 2 && gameBoard[9] === 2) {
+        document.getElementById("status").innerHTML = "Red Wins!"
+        gameStatus = 1
+        redWins += 1
+    }
+    else if (gameBoard[1] === 2 && gameBoard[5] === 2 && gameBoard[9] === 2) {
+        document.getElementById("status").innerHTML = "Red Wins!"
+        gameStatus = 1
+        redWins += 1
+    }
+    else if (gameBoard[3] === 2 && gameBoard[5] === 2 && gameBoard[7] === 2) {
+        document.getElementById("status").innerHTML = "Red Wins!"
+        gameStatus = 1
+        redWins += 1
+    }
+}
+
+function drawCheck() {
+    if (gameBoard[1] != 0 && gameBoard[2] != 0 && gameBoard[3] != 0 && gameBoard[4] != 0 && gameBoard[5] != 0 && gameBoard[6] != 0 && gameBoard[7] != 0 && gameBoard[8] != 0 && gameBoard[9] != 0) {
+        document.getElementById("status").innerHTML = "Draw"
+    }
+}
+
+function updateScore() {
+    document.getElementById("blueScoreboard").innerHTML = blueWins
+    document.getElementById("redScoreboard").innerHTML = redWins
+}
+
+function newGame() {
+    defaultColor = "lightgreen"
+    gameStatus = 0
+    gameBoard = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    document.getElementById("T1").style = "background-color: " + defaultColor + ";"
+    document.getElementById("T2").style = "background-color: " + defaultColor + ";"
+    document.getElementById("T3").style = "background-color: " + defaultColor + ";"
+    document.getElementById("M1").style = "background-color: " + defaultColor + ";"
+    document.getElementById("M2").style = "background-color: " + defaultColor + ";"
+    document.getElementById("M3").style = "background-color: " + defaultColor + ";"
+    document.getElementById("B1").style = "background-color: " + defaultColor + ";"
+    document.getElementById("B2").style = "background-color: " + defaultColor + ";"
+    document.getElementById("B3").style = "background-color: " + defaultColor + ";"
+    if (turn === 1) {
+        document.getElementById("status").innerHTML = "Red's Turn"
+    }
+    else if (turn === 0) {
+        document.getElementById("status").innerHTML = "Blue's Turn"
+    }
+}
+
+function reset() {
+    redWins = 0
+    blueWins = 0
+    updateScore()
+    newGame()
+}
