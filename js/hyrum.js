eebe853fe111a7fdd5fd79e936b2771aca8570af
HyrumTew <lemon_bars@hotmail.com>
2016-02-16 19:07:21 -0800

    Made some circles
    made the circles move across the screen and added a timer.  weird bug
    circles sit under the start button after there animation is done

--- js/hyrum.js
+++ js/hyrum.js
@@ -1,4 +1,28 @@
-function circle() {
-	document.getElementById('cp').innerHTML =
-	'<svg height="100" width="100"><circle cx="50" cy="50" r="40"stroke="purple" stroke-width="10" fill="red" />'
+function random() {
+	var rand = Math.floor((Math.random()* 1500 ) + 1000)
+	setTimeout(circleFunction, rand)
+}
+
+
+
+function circleFunction() {
+	var svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+	svg.setAttribute("width", "100");
+	svg.setAttribute("height", "100");
+	document.getElementById("circleDiv").appendChild(svg); 
+
+	var att = document.createAttribute("class");
+	att.value = "circles";
+	svg.setAttributeNode(att);
+
+	var circle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+	circle.setAttribute("cx", "50");
+	circle.setAttribute("cy", "50");
+	circle.setAttribute("r", "40");
+	circle.setAttribute("stroke", "black");
+	circle.setAttribute("stroke-width", "3px");
+	circle.setAttribute("fill", "red");
+
+	svg.appendChild(circle);
+	random()
 }
