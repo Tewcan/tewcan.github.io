7099b0d8c9de3d19270be614b565812417c18b33
HyrumTew <lemon_bars@hotmail.com>
2016-02-20 03:43:33 -0800

    Added background images
    Made a background image for index.html and hyrum.html

--- js/js stuff.js
+++ js/js stuff.js
@@ -3,14 +3,14 @@ var d = new Date()
 var hour = d.getHours()
 function output (text,id) {
 	if (id === undefined) id = 'time__msg'
-	document.getElementById(id).innerHTML = text
+	document.getElementsByClassName(id)[0].innerHTML = text
 
 }
 function checkTime (a,b) {
 	return hour >= a && hour < b
 }
 function time () {
-	output(d.toDateString(),'block__button');
+	output(d.toDateString(),'time__button');
 	if ( checkTime(5,11) ) output("Good Morning");
 	else if ( checkTime(11,16) ) output("Good Afternoon");
 	else if ( checkTime(16,20) ) output("Good Evening");
@@ -20,16 +20,17 @@ function time () {
 var n = 0
 function count() {
 	n++
-	document.getElementById("count__number").innerHTML = n
+	document.getElementsByClassName("count__number")[0].innerHTML = n
 }
 // Text Creator function
 function textCreator() {
-	var txt = document.getElementById('text__box').value;
-	var para = document.createElement('h3');
-	var text = document.createTextNode(txt);
-	para.onclick = strikeThrough
-	para.appendChild(text)
-	document.getElementById('text__div').insertBefore(para, document.getElementById('text__div').childNodes[0]);
+	var element = document.createElement('h3');
+	var text = document.getElementsByClassName('text__box')[0].value;
+	var textNode = document.createTextNode(text);
+	element.appendChild(textNode)
+	element.onclick = strikeThrough
+	var textDiv = document.getElementsByClassName('text__div')[0]
+	textDiv.insertBefore(element, textDiv.childNodes[0]);
 }
 
 function strikeThrough () {
