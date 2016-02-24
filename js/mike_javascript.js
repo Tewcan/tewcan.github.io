66242cdf1d570631602e8fb659322341a03f36ae
Mike-Tew <Mike.Tew@gmail.com>
2016-02-24 13:13:22 -0800

    Implemented factorials

--- js/mike_javascript.js
+++ js/mike_javascript.js
@@ -1,23 +1,12 @@
-function fact() {
-	var n = document.getElementById('n');
-	var n = n.value;
-	if (n == 0 || n == 1) {
-		alert('1');
-	} else {
-		return n * fact(n - 1);
-	}
-}
-
 function factDos() {
+	var no = Number(document.getElementById('n').value);
 	var fact = 1;
-	var no = document.getElementById('n');
-	var no = n.value;
-	var no = parseInt(n);
 	var i = 1;
 	for(i = 1; i <= no; i ++) {
 		fact = fact * i;
 	}
-	console.log(i);
+	document.getElementById('facAnswer').value = fact;
+	console.log(fact);
 }
 
 function fib() {
@@ -25,5 +14,5 @@ function fib() {
 	var f = f.value;
 	var g = f - 1;
 	var f = (parseInt(f)) + (parseInt(g));
-	document.getElementById('answer').value = f;
+	document.getElementById('fibAnswer').value = f;
 }
