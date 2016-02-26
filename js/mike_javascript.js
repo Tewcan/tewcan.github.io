f5051a3a9010c82f0e2aa5ddfce888f504e4c01a
Mike-Tew <Mike.Tew@gmail.com>
2016-02-26 03:12:01 -0800

    Created Mike.html and subpages
    Created mike.html
    Created mike_bio.html
    Created mike_random.html
    Updated Fibonacci/Factorial web page

--- js/mike_javascript.js
+++ js/mike_javascript.js
@@ -1,18 +1,29 @@
-function factDos() {
+//factorial function
+function factDos() {  
+	document.getElementById('facIter').innerHTML = '';
 	var no = Number(document.getElementById('n').value);
 	var fact = 1;
 	var i = 1;
 	for(i = 1; i <= no; i ++) {
 		fact = fact * i;
+		document.getElementById('facIter').innerHTML += i;
+		document.getElementById('facIter').innerHTML += ' * ';
 	}
+	var temp = document.getElementById('facIter').innerHTML.slice(0, -2);
+	console.log(temp);
+	document.getElementById('facIter').innerHTML = temp + '= ';
 	document.getElementById('facAnswer').value = fact;
-	console.log(fact);
+	document.getElementById('facIter').innerHTML += fact;
 }
 
-function fib() {
+// fibonacci function
+function fib() {   
 	var f = document.getElementById('f');
 	var f = f.value;
 	var g = f - 1;
-	var f = (parseInt(f)) + (parseInt(g));
-	document.getElementById('fibAnswer').value = f;
+	var h = (parseInt(f)) + (parseInt(g));
+	document.getElementById('fibAnswer').value = h;
+	document.getElementById('fibIter').innerHTML = f + ' + ' + g + ' = ' + h;
 }
+
+
