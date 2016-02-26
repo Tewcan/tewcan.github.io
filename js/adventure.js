0efe0151cd59654aab44b8b0f3274e63c5c385e2
HyrumTew <lemon_bars@hotmail.com>
2016-02-26 13:17:24 -0800

    Adventure Game
    Made the map better
    Added room names
    Made room buttons disappear  at dead ends

--- js/adventure.js
+++ js/adventure.js
@@ -1,9 +1,7 @@
 //Game Variables
-var room = 1
 var life = 5
 var monsterLife = 3
 var gameState
-
 //Other Variables
 var getMap = document.getElementsByClassName('mapimage')
 
@@ -11,59 +9,99 @@ var getMap = document.getElementsByClassName('mapimage')
 function roomSelecter(input) {
 	if (gameState === 'stopped') {return}
 	//if (life <= 0) {return}
-	if (input === 1) {firstRoom()}
-	else if (input === 2) {secondRoom()}
-	else if(input === 3) {thirdRoom()}
-	else if(input === 4) {fourthRoom()}
+	if (input === room[0]) {firstRoom()}
+	else if (input === room[1]) {secondRoom()}
+	else if(input === room[2]) {thirdRoom()}
+	else if(input === room[3]) {fourthRoom()}
+	else if(input === room[4]) {room5()}
 }
 function firstRoom(text) {
-	room = 1
+	currentRoom = room[0]
 	life = 5
-	roomButtonChanger(2,3)
-	textBox("Healing fountain",'orange')
-	getMap[0].setAttribute('src','images/adventure/r1.jpg')
+	//theRoomFunction(room[1],'','orange','-1115px -103')
+	roomButtonChanger(room[1])
+	textBox("",'orange')
+	changeMap('-1115px -103px')
 	var spawnChance = Math.floor((Math.random() * 10))
 }
 function secondRoom(text) {
-	room = 2
-	roomButtonChanger(1)
-	textBox("Random dead end",'orange')
-	getMap[0].setAttribute('src','images/adventure/r2.jpg')
+	currentRoom = room[1]
+	roomButtonChanger(room[2],room[0])
+	textBox("",'orange')
+	changeMap('-1125px -240px')
 	var spawnChance = Math.floor((Math.random() * 10))
-	if (spawnChance <= 3) {
-		textBox('Entering combat','red')
-		combat()
-	}
 }
 function thirdRoom(text) {
-	room = 3
-	roomButtonChanger(1,4)
-	textBox("The great hallway",'orange')
-	getMap[0].setAttribute('src','images/adventure/r3.jpg')
+	currentRoom = room[2]
+	roomButtonChanger(room[3],room[1])
+	textBox("",'orange')
+	changeMap('-1115px -380px')
 	var spawnChance = Math.floor((Math.random() * 10))
 }
 function fourthRoom(text) {
-	room = 4
-	roomButtonChanger(3)
-	textBox("Careful of monsters",'orange')
-	getMap[0].setAttribute('src','images/adventure/r4.jpg')
+	currentRoom = room[3]
+	roomButtonChanger(room[4],room[2])
+	textBox("Careful of monsters",'lightblue')
+	changeMap('-1046px -550px')
 	var spawnChance = Math.floor((Math.random() * 10))
 	if (spawnChance <= 3) {
-		textBox('Entering combat','red')
+		textBox('Entering combat','red','25px')
 		combat()
 	}
 }
-function roomButtonChanger(button1,button2) {
-	updateDisplay()
-	if (button1 === undefined) {button1 = 0}
-	if (button2 === undefined) {button2 = 0}
-	document.getElementsByClassName('firstroombutton')[0].onclick = function() {roomSelecter(button1)}
-	document.getElementsByClassName('firstroombutton')[0].innerHTML = "Room " + button1
-	document.getElementsByClassName('secondroombutton')[0].onclick = function() {roomSelecter(button2)}
-	document.getElementsByClassName('secondroombutton')[0].innerHTML = "Room " + button2
+function room5(text) {
+	currentRoom = room[3]
+	roomButtonChanger(room[3])
+	textBox("We've almost made it",'lightblue')
+	changeMap('-972px -702px')
+	var spawnChance = Math.floor((Math.random() * 10))
+	if (spawnChance <= 3) {
+		textBox('Entering combat','red','25px')
+		combat()
+	}
 }
+function roomButtonChanger(button1,button2,button3,button4) {
+	updateDisplay()
+	var but1 = document.getElementsByClassName('adventurebutton')[1]
+	var but2 = document.getElementsByClassName('adventurebutton')[2]
+	var but3 = document.getElementsByClassName('adventurebutton')[3]
+	var but4 = document.getElementsByClassName('adventurebutton')[4]
+	if (button1 === undefined) {but1.style.display = 'none'}
+		else {
+			but1.style.display ='initial'
+			but1.onclick = function() {roomSelecter(button1)}
+			but1.innerHTML = button1
+		}
+	if (button2 === undefined) {but2.style.display = 'none'}
+		else {
+			but2.style.display ='initial'
+			but2.onclick = function() {roomSelecter(button2)}
+			but2.innerHTML = button2
+		}
+	if (button3 === undefined) {but3.style.display = 'none'}
+		else {
+			but3.style.display ='initial'
+			but3.onclick = function() {roomSelecter(button2)}
+			but3.innerHTML = button2
+		}
+	if (button4 === undefined) {but4.style.display = 'none'}
+		else {
+			but4.style.display ='initial'
+			but4.onclick = function() {roomSelecter(button2)}
+			but4.innerHTML = button2
+		}
+}
+/*function theRoomFunction(r1,r2,r3,r4,text,color,position) {
+	roomButtonChanger(r1,r2,r3,r4)
+	textBox(text,color)
+	changeMap(position)
+}*/
 
 //Other Functions
+function changeMap(position) {
+	document.getElementsByClassName('mapimage')[0].style.backgroundPosition = position
+	document.getElementsByClassName('roomnum')[0].innerHTML = currentRoom
+}
 function combat() {
 	gameState = 'stopped'
 	document.getElementsByClassName('attack')[0].onclick = function() {combat()}
@@ -71,27 +109,29 @@ function combat() {
 	var monsterAttack = Math.floor((Math.random() * 11))
 	if (playerAttack <= 5) {
 		monsterLife--
-		textBox('You hit the monster','','15px')
+		textBox('You hit the monster','','18px')
 	}
 	else(textBox('You miss','yellow','15px'))
 	if (monsterAttack <= 5) {
 		life--
-		textBox('The monster hit you','red','15px')
+		textBox('The monster hit you','red','18px')
 	}
 	else (textBox('You dodged','blue','15px'))
 	if (life <= 0) {
-		textBox('You Died','red','25px')
+		textBox('You Died','red','30px')
 		updateDisplay('leftcombat')
 	}
 	if (monsterLife <= 0) {
-		textBox('The monster Died')
+		textBox('The monster Died','','25px')
 		updateDisplay('leftcombat')
 		gameState = 'started'
 		monsterLife = 3
 	}
 }
+//function flee () {
+//	gameState = 'started'
+//}
 function updateDisplay(input) {
-	document.getElementsByClassName('roomnum')[0].innerHTML = 'Room ' + room
 	document.getElementsByClassName('life')[0].innerHTML = 'Life: ' + life
 	if (input === 'leftcombat') {
 		document.getElementsByClassName('attack')[0].onclick = function() {}
@@ -114,4 +154,14 @@ function clearTextBox () {
     	textbox.removeChild(textbox.firstChild);
 	}
 }
+function mathStuff(width,height) {
+	width -= 225
+	height -= 160
+	textBox(height)
+	textBox(width)
+}
 
+
+
+
+var room = ['Inside the Barrow','Narrow Tunnel','Foot Bridge','Great Cavern','Shallow Ford']
