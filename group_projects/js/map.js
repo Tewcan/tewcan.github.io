204e6d1a8cd57a942a542358cc02e28e087ebbe2
HyrumTew <lemon_bars@hotmail.com>
2016-03-08 17:31:27 -0800

    Group Project
    Added first attempt at movement system

--- group_projects/js/map.js
+++ group_projects/js/map.js
@@ -1,46 +1,36 @@
+var hero = {
+    x: 250,
+    y: 250
+}
+function moveHero() {
+    var domHerox = document.getElementsByClassName('hero')[0].style.left = hero.x + 'px'
+    var domHeroy = document.getElementsByClassName('hero')[0].style.top = hero.y + 'px'
+}
 
-            function leftArrowPressed() {
-            var element = document.getElementById("charater");
-            element.style.left = parseInt(element.style.left) - 5 + 'px';
-            }
+function moveSelection(evt) {
+    switch (evt.keyCode) {
+        case 37: //left
+            if (hero.x <=30) {}
+            else {hero.x -= 5}
+        break;
+        case 39: //right
+            if (hero.x >=620) {}
+            else {hero.x += 5}
+        break;
+        case 38: //up
+            if (hero.y <=30) {}
+            else {hero.y -= 5}
+        break;
+        case 40: //down
+            if (hero.y >=430) {}
+            else {hero.y += 5}
+        break;
+    }
+};
 
-            function rightArrowPressed() {
-            var element = document.getElementById("charater");
-            element.style.left = parseInt(element.style.left) + 5 + 'px';
-
-            }
-
-            function upArrowPressed() {
-            var element = document.getElementById("charater");
-            element.style.top = parseInt(element.style.top) - 5 + 'px';
-            }
-
-            function downArrowPressed() {
-            var element = document.getElementById("charater");
-            element.style.top = parseInt(element.style.top) + 5 + 'px';
-            }
-
-            function moveSelection(evt) {
-                switch (evt.keyCode) {
-                    case 37:
-                    leftArrowPressed();
-                    break;
-                    case 39:
-                    rightArrowPressed();
-                    break;
-                    case 38:
-                    upArrowPressed();
-                    break;
-                    case 40:
-                    downArrowPressed();
-                    break;
-                    }
-                };
-
-        function docReady()
-        {
-
-          window.addEventListener('keydown', moveSelection);
-        }
-// onload="docReady()" onkeydown="" onkeyup=""
-// <img id="charater" src="../images/logo.png" style="position:absolute;left:0; top:0;" height="15" width="15">
+function docReady(){
+    window.addEventListener('keydown', moveSelection);
+}
+setInterval(function(){
+    moveHero()
+}, 10);
