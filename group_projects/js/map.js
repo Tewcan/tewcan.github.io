b3a379a97e8e48c7d397b59ffff7ae1d978bf7ec
HyrumTew <lemon_bars@hotmail.com>
2016-03-12 02:18:27 -0800

    Group Project
    Made collision work better
    Added the ability to create bushes

--- group_projects/js/map.js
+++ group_projects/js/map.js
@@ -1,8 +1,8 @@
 function mapFunction() {
 var keyBoard = {}
 var hero = {
-    x: 250,
-    y: 250
+    x: 80,
+    y: 40
 }
 var randomX = Math.floor((Math.random() * 550) + 30)
 var randomY = Math.floor((Math.random() * 350) + 30)
@@ -10,10 +10,21 @@ var enemy = {
     x: randomX,
     y: randomY
 }
-var bush = {
-    x: 150,
-    y: 200
-}
+var exit = {x:650,y:350}
+
+var bushleft = {x:0,y:0,w:50,h:450}
+var bushright = {x:650,y:0,w:50,h:355}
+var bushtop = {x:0,y:0,w:700,h:50}
+var bushbottom = {x:0,y:450,w:700,h:50}
+var bush0 = {x:150,y:50,w:50,h:150}
+var bush1 = {x:150,y:250,w:300,h:50}
+var bush2 = {x:317,y:91,w:50,h:50}
+var bush3 = {x:400,y:150,w:250,h:50}
+var bush4 = {x:500,y:350,w:100,h:50}
+var bush5 = {x:420,y:330,w:50,h:50}
+var bush6 = {x:150,y:400,w:50,h:50}
+var layout = [bushleft,bushright,bushtop,bushbottom,bush0,bush1,bush2,bush3,bush4,bush5,bush6]
+
 addEventListener('keydown', function(input) {
     keyBoard[input.keyCode] = true
 },false);
@@ -23,39 +34,51 @@ addEventListener('keyup', function(input) {
 
 function gameUpdate() {
     if (37 in keyBoard) { //left
-        if (hero.x <= (bush.x + 383) && bush.x <= (hero.x + 28) &&
-            hero.y <= (bush.y + 25) && bush.y <= (hero.y + 25)) {}
-        else if (hero.x < 30) {}
-        else {hero.x -= 2}}
+        if (collisionFunciton(3,0,0,0) === true) {}
+        else {hero.x -= 3}}
     if (39 in keyBoard) { //right
-        if (hero.x <= (bush.x + 380) && bush.x <= (hero.x + 25) &&
-            hero.y <= (bush.y + 25) && bush.y <= (hero.y + 25)) {hero.x -= 8}
-            else if (hero.x > 620) {}
-        else {hero.x += 2}}
+        if (collisionFunciton(0,3,0,0) === true) {}
+        else {hero.x += 3}}
     if (38 in keyBoard) { //up
-        if (hero.x <= (bush.x + 380) && bush.x <= (hero.x + 25) &&
-            hero.y <= (bush.y + 28) && bush.y <= (hero.y + 28)) {}
-            else if (hero.y < 30) {}
-        else {hero.y -= 2}}
+        if (collisionFunciton(0,0,3,0) === true) {}
+        else {hero.y -= 3}}
     if (40 in keyBoard) { //down
-        if (hero.x <= (bush.x + 380) && bush.x <= (hero.x + 25) &&
-            hero.y <= (bush.y + 25) && bush.y <= (hero.y + 25)) {hero.y -= 8}
-            else if (hero.y > 430) {}
-        else {hero.y += 2}}
+        if (collisionFunciton(0,0,0,3) === true) {}
+        else {hero.y += 3}}
+    function collisionFunciton(left,right,up,down) {
+        for (var i = 0; i < layout.length; i++) {
+            if (hero.x <= (layout[i].x + (layout[i].w - 20) + left) && layout[i].x <= (hero.x + 25 + right) &&
+                hero.y <= (layout[i].y + (layout[i].h - 20) + up) && layout[i].y <= (hero.y + 25 + down)) {return true}
+        }
+    }
 
     //player and monster collide
     if (hero.x <= (enemy.x + 25) && enemy.x <= (hero.x + 25) &&
         hero.y <= (enemy.y + 25) && enemy.y <= (hero.y + 25) ) {
         stopTimer()
         alert('combat') }
+    if (hero.x <= (exit.x + 25) && exit.x <= (hero.x + 25) &&
+        hero.y <= (exit.y + 25) && exit.y <= (hero.y + 25) ) {
+        document.getElementsByTagName('body')[0].style.backgroundColor = 'red' }
 
     document.getElementsByClassName('enemy')[0].style.left = enemy.x + 'px'
     document.getElementsByClassName('enemy')[0].style.top = enemy.y + 'px'
     document.getElementsByClassName('hero')[0].style.left = hero.x + 'px'
     document.getElementsByClassName('hero')[0].style.top = hero.y + 'px'
-    document.getElementsByClassName('bush')[0].style.left = bush.x + 'px'
-    document.getElementsByClassName('bush')[0].style.top = bush.y + 'px'
 };
+function updateMap() {
+    for (var i = 0; i < layout.length; i++) {
+        var bushDiv = document.createElement('div')
+        bushDiv.setAttribute('class','bush')
+        var heroDiv = document.getElementsByClassName('map')[0]
+        heroDiv.appendChild(bushDiv)
+        document.getElementsByClassName('bush')[i].style.left = layout[i].x + 'px'
+        document.getElementsByClassName('bush')[i].style.top = layout[i].y + 'px'
+        document.getElementsByClassName('bush')[i].style.width = layout[i].w + 'px'
+        document.getElementsByClassName('bush')[i].style.height = layout[i].h + 'px'
+    }
+}
+updateMap()
 var gameTimer = setInterval(function(){
     gameUpdate()
 }, 20);
