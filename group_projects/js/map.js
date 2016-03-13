44108fa44591dcdf4b6bb2af2719410b7259a53e
HyrumTew <lemon_bars@hotmail.com>
2016-03-12 20:03:15 -0800

    testing for conflict
    asd

--- group_projects/js/map.js
+++ group_projects/js/map.js
@@ -1,8 +1,7 @@
 function mapFunction() {
-var keyBoard = {}
 var hero = {
-    x: 80,
-    y: 40
+    x: 600,
+    y: 400
 }
 var randomX = Math.floor((Math.random() * 550) + 30)
 var randomY = Math.floor((Math.random() * 350) + 30)
@@ -10,28 +9,24 @@ var enemy = {
     x: randomX,
     y: randomY
 }
-var exit = {x:650,y:350}
 
-var bushleft = {x:0,y:0,w:50,h:450}
-var bushright = {x:650,y:0,w:50,h:355}
-var bushtop = {x:0,y:0,w:700,h:50}
-var bushbottom = {x:0,y:450,w:700,h:50}
-var bush0 = {x:150,y:50,w:50,h:150}
-var bush1 = {x:150,y:250,w:300,h:50}
-var bush2 = {x:317,y:91,w:50,h:50}
-var bush3 = {x:400,y:150,w:250,h:50}
-var bush4 = {x:500,y:350,w:100,h:50}
-var bush5 = {x:420,y:330,w:50,h:50}
-var bush6 = {x:150,y:400,w:50,h:50}
-var layout = [bushleft,bushright,bushtop,bushbottom,bush0,bush1,bush2,bush3,bush4,bush5,bush6]
+//Rooms
+var currentRoom = 0
+var layout0 = [entrance = {nx:600,ny:350},exit = {x:650,y:350,w:50,h:110,r:1,class:'exit'},bushleft = {x:0,y:0,w:50,h:450},bushright = {x:650,y:0,w:50,h:355},bushtop = {x:0,y:0,w:700,h:50},bushbottom = {x:0,y:450,w:700,h:50},bush0 = {x:150,y:50,w:50,h:150},
+    bush1 = {x:150,y:250,w:300,h:50,class:'water'},bush2 = {x:317,y:91,w:50,h:50},bush3 = {x:400,y:150,w:250,h:50},bush4 = {x:500,y:350,w:100,h:50},bush5 = {x:420,y:330,w:50,h:50},bush6 = {x:150,y:400,w:50,h:50}]
+var layout1 = [entrance = {x:0,y:350,nx:50,ny:350,w:50,h:110,r:0,class:'entrance'},exit = {x:300,y:0,w:100,h:50,r:2,class:'exit'},bushleft = {x:0,y:0,w:50,h:350},bushright = {x:650,y:0,w:50,h:450},bushtop = {x:0,y:0,w:300,h:50},bushbottom = {x:0,y:450,w:700,h:50},bush0 = {x:150,y:50,w:50,h:150},
+    bush1 = {x:150,y:250,w:300,h:50},bush2 = {x:317,y:91,w:50,h:50},bush3 = {x:400,y:150,w:250,h:50},bush4 = {x:500,y:350,w:100,h:50},bush5 = {x:420,y:330,w:50,h:50},bush6 = {x:400,y:0,w:300,h:50}]
+var layout2 = [entrance = {x:300,y:0,nx:100,ny:100,w:100,h:50,r:1,class:'entrance'},exit = {x:0,y:0,w:0,h:0,r:2,class:'exit'},bushleft = {x:0,y:0,w:50,h:350},bushright = {x:650,y:0,w:50,h:450},bushtop = {x:0,y:0,w:300,h:50},bushbottom = {x:0,y:450,w:700,h:50},bush0 = {x:150,y:50,w:50,h:150},
+    bush1 = {x:150,y:250,w:300,h:50},bush2 = {x:317,y:91,w:50,h:50},bush3 = {x:400,y:150,w:250,h:50},bush4 = {x:500,y:350,w:100,h:50},bush5 = {x:420,y:330,w:50,h:50},bush6 = {x:400,y:0,w:300,h:50}]
+var rooms = [layout0,layout1,layout2]
 
+var keyBoard = {}
 addEventListener('keydown', function(input) {
     keyBoard[input.keyCode] = true
 },false);
 addEventListener('keyup', function(input) {
     delete keyBoard[input.keyCode]
 },false);
-
 function gameUpdate() {
     if (37 in keyBoard) { //left
         if (collisionFunciton(3,0,0,0) === true) {}
@@ -46,43 +41,66 @@ function gameUpdate() {
         if (collisionFunciton(0,0,0,3) === true) {}
         else {hero.y += 3}}
     function collisionFunciton(left,right,up,down) {
-        for (var i = 0; i < layout.length; i++) {
-            if (hero.x <= (layout[i].x + (layout[i].w - 20) + left) && layout[i].x <= (hero.x + 25 + right) &&
-                hero.y <= (layout[i].y + (layout[i].h - 20) + up) && layout[i].y <= (hero.y + 25 + down)) {return true}
+        for (var i = 0; i < rooms[currentRoom].length; i++) {
+            if (hero.x <= (rooms[currentRoom][i].x + (rooms[currentRoom][i].w - 20) + left) &&
+                rooms[currentRoom][i].x <= (hero.x + 30 + right) &&
+                hero.y <= (rooms[currentRoom][i].y + (rooms[currentRoom][i].h - 35) + up) &&
+                rooms[currentRoom][i].y <= (hero.y + 45 + down)) {
+                if (rooms[currentRoom][i].class === 'entrance') { //Entrance
+                    currentRoom = rooms[currentRoom][0].r
+                    hero.x = rooms[currentRoom][0].nx
+                    hero.y = rooms[currentRoom][0].ny
+                    console.log(hero.x, currentRoom)
+                    mapUpdate()
+                    return false}
+                if (rooms[currentRoom][i].class === 'exit') { //Exit
+                    currentRoom = rooms[currentRoom][1].r
+                    hero.x = rooms[currentRoom][0].nx
+                    hero.y = rooms[currentRoom][0].ny
+                    console.log(hero.x, currentRoom)
+                    mapUpdate()
+                    return false}
+                else {return true}
+            }
         }
     }
 
     //player and monster collide
     if (hero.x <= (enemy.x + 25) && enemy.x <= (hero.x + 25) &&
         hero.y <= (enemy.y + 25) && enemy.y <= (hero.y + 25) ) {
-        stopTimer()
+        stopGameTimer()
         alert('combat') }
-    if (hero.x <= (exit.x + 25) && exit.x <= (hero.x + 25) &&
-        hero.y <= (exit.y + 25) && exit.y <= (hero.y + 25) ) {
-        document.getElementsByTagName('body')[0].style.backgroundColor = 'red' }
 
     document.getElementsByClassName('enemy')[0].style.left = enemy.x + 'px'
     document.getElementsByClassName('enemy')[0].style.top = enemy.y + 'px'
     document.getElementsByClassName('hero')[0].style.left = hero.x + 'px'
     document.getElementsByClassName('hero')[0].style.top = hero.y + 'px'
 };
-function updateMap() {
-    for (var i = 0; i < layout.length; i++) {
+
+function mapUpdate() {
+    var tile = document.getElementsByClassName('tile')
+    var mapDiv = document.getElementsByClassName('map__tiles')[0]
+    while (mapDiv.firstChild) {
+        mapDiv.removeChild(mapDiv.firstChild)}
+    for (var i = 0; i < rooms[currentRoom].length; i++) {
+        var classImage = rooms[currentRoom][i].class
+        if (classImage === undefined) { classImage = 'bush'}
+
         var bushDiv = document.createElement('div')
-        bushDiv.setAttribute('class','bush')
-        var heroDiv = document.getElementsByClassName('map')[0]
-        heroDiv.appendChild(bushDiv)
-        document.getElementsByClassName('bush')[i].style.left = layout[i].x + 'px'
-        document.getElementsByClassName('bush')[i].style.top = layout[i].y + 'px'
-        document.getElementsByClassName('bush')[i].style.width = layout[i].w + 'px'
-        document.getElementsByClassName('bush')[i].style.height = layout[i].h + 'px'
+            bushDiv.setAttribute('class','tile ')
+            mapDiv.appendChild(bushDiv)
+        tile[i].style.left = rooms[currentRoom][i].x + 'px'
+        tile[i].style.top = rooms[currentRoom][i].y + 'px'
+        tile[i].style.width = rooms[currentRoom][i].w + 'px'
+        tile[i].style.height = rooms[currentRoom][i].h + 'px'
+        tile[i].className += classImage
     }
 }
-updateMap()
+mapUpdate()
 var gameTimer = setInterval(function(){
     gameUpdate()
 }, 20);
-function stopTimer() {
+function stopGameTimer() {
     clearInterval(gameTimer)
 }
 
