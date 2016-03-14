767004df9405e300b511b198bd7bea8d0d06ec05
HyrumTew <lemon_bars@hotmail.com>
2016-03-13 20:39:20 -0700

    asd

--- group_projects/js/map.js
+++ group_projects/js/map.js
@@ -12,12 +12,12 @@ var enemy = {
 
 //Rooms
 var currentRoom = 0
-var layout0 = [entrance = {nx:600,ny:350},exit = {x:650,y:350,w:50,h:110,r:1,class:'exit'},bushleft = {x:0,y:0,w:50,h:450},bushright = {x:650,y:0,w:50,h:355},bushtop = {x:0,y:0,w:700,h:50},bushbottom = {x:0,y:450,w:700,h:50},bush0 = {x:150,y:50,w:50,h:150},
-    bush1 = {x:150,y:250,w:300,h:50,class:'water'},bush2 = {x:317,y:91,w:50,h:50},bush3 = {x:400,y:150,w:250,h:50},bush4 = {x:500,y:350,w:100,h:50},bush5 = {x:420,y:330,w:50,h:50},bush6 = {x:150,y:400,w:50,h:50}]
-var layout1 = [entrance = {x:0,y:350,nx:50,ny:350,w:50,h:110,r:0,class:'entrance'},exit = {x:300,y:0,w:100,h:50,r:2,class:'exit'},bushleft = {x:0,y:0,w:50,h:350},bushright = {x:650,y:0,w:50,h:450},bushtop = {x:0,y:0,w:300,h:50},bushbottom = {x:0,y:450,w:700,h:50},bush0 = {x:150,y:50,w:50,h:150},
-    bush1 = {x:150,y:250,w:300,h:50},bush2 = {x:317,y:91,w:50,h:50},bush3 = {x:400,y:150,w:250,h:50},bush4 = {x:500,y:350,w:100,h:50},bush5 = {x:420,y:330,w:50,h:50},bush6 = {x:400,y:0,w:300,h:50}]
-var layout2 = [entrance = {x:300,y:0,nx:300,ny:50,w:100,h:50,r:1,class:'entrance'},exit = {x:0,y:0,w:0,h:0,r:2,class:'exit'},bushleft = {x:0,y:0,w:50,h:450},bushright = {x:650,y:0,w:50,h:450},bushtop = {x:0,y:0,w:300,h:50},bushbottom = {x:0,y:450,w:700,h:50},bush0 = {x:150,y:50,w:50,h:150},
-    bush1 = {x:150,y:250,w:300,h:50},bush2 = {x:317,y:110,w:50,h:50},bush3 = {x:400,y:150,w:250,h:50},bush4 = {x:500,y:350,w:100,h:50},bush5 = {x:420,y:330,w:50,h:50},bush6 = {x:400,y:0,w:300,h:50}]
+var layout0 = [entrance = {nx:600,ny:350},exit = {x:650,y:350,nx:600,ny:350,w:50,h:110,r:1,class:'exit'},bushleft = {x:0,y:0,w:50,h:450},bushright = {x:650,y:0,w:50,h:355},bushtop = {x:0,y:0,w:700,h:50},bushbottom = {x:0,y:450,w:700,h:50},
+    bush0 = {x:150,y:50,w:50,h:150},bush1 = {x:150,y:250,w:300,h:50,class:'water'},bush2 = {x:317,y:91,w:50,h:50},bush3 = {x:400,y:150,w:250,h:50},bush4 = {x:500,y:350,w:100,h:50},bush5 = {x:420,y:330,w:50,h:50},bush6 = {x:150,y:400,w:50,h:50}]
+var layout1 = [entrance = {x:0,y:351,nx:50,ny:350,w:50,h:110,r:0,class:'entrance'},exit = {x:300,y:0,nx:300,ny:50,w:100,h:50,r:2,class:'exit'},bushleft = {x:0,y:0,w:50,h:350},bushright = {x:650,y:0,w:50,h:450},bushtop = {x:0,y:0,w:300,h:50},bushbottom = {x:0,y:450,w:700,h:50},
+    bush0 = {x:150,y:50,w:50,h:150},bush1 = {x:150,y:250,w:300,h:50},bush2 = {x:317,y:110,w:50,h:50},bush3 = {x:400,y:150,w:250,h:50},bush4 = {x:500,y:350,w:100,h:50},bush5 = {x:420,y:330,w:50,h:50},bush6 = {x:400,y:0,w:300,h:50}]
+var layout2 = [entrance = {x:300,y:450,nx:300,ny:400,w:100,h:50,r:1,class:'entrance'},exit = {x:300,y:0,nx:300,ny:300,w:100,h:50,r:2,class:'exit'},bushleft = {x:0,y:0,w:50,h:450},bushright = {x:650,y:0,w:50,h:450},bushtop = {x:0,y:0,w:300,h:50},bushbottom = {x:0,y:450,w:300,h:50},
+    bush0 = {x:150,y:50,w:50,h:150},bush1 = {x:150,y:250,w:300,h:50},bush2 = {x:400,y:450,w:300,h:50},bush3 = {x:400,y:150,w:250,h:50},bush4 = {x:500,y:350,w:100,h:50},bush5 = {x:420,y:330,w:50,h:50},bush6 = {x:400,y:0,w:300,h:50}]
 var rooms = [layout0,layout1,layout2]
 
 var keyBoard = {}
@@ -48,34 +48,31 @@ function gameUpdate() {
                 rooms[currentRoom][i].y <= (hero.y + 45 + down)) {
                 if (rooms[currentRoom][i].class === 'entrance') { //Entrance
                     currentRoom = rooms[currentRoom][0].r
-                    hero.x = rooms[currentRoom][0].nx
-                    hero.y = rooms[currentRoom][0].ny
-                    console.log(hero.x, currentRoom)
+                    hero.x = rooms[currentRoom][1].nx
+                    hero.y = rooms[currentRoom][1].ny
                     mapUpdate()
                     return false}
                 if (rooms[currentRoom][i].class === 'exit') { //Exit
                     currentRoom = rooms[currentRoom][1].r
                     hero.x = rooms[currentRoom][0].nx
                     hero.y = rooms[currentRoom][0].ny
-                    console.log(hero.x, currentRoom)
                     mapUpdate()
                     return false}
-                if (rooms[currentRoom][i].class === 'enemy') { //player and monster collide
-
-                    return false
-                }
+                // if (rooms[currentRoom][i].class === 'enemy') { //player and monster collide
+                    // stopGameTimer()
+                    // beginCombat()
+                    // return false
+                // }
                 else {return true}
             }
         }
     }
-
     //player and monster collide
     if (hero.x <= (enemy.x + 25) && enemy.x <= (hero.x + 25) &&
         hero.y <= (enemy.y + 25) && enemy.y <= (hero.y + 25) ) {
         stopGameTimer()
         beginCombat()
     }
-
     document.getElementsByClassName('enemy')[0].style.left = enemy.x + 'px'
     document.getElementsByClassName('enemy')[0].style.top = enemy.y + 'px'
     document.getElementsByClassName('hero')[0].style.left = hero.x + 'px'
