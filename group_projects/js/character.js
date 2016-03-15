246c169232fa0dec26879a3efa23695560011810
Williamtew <william@nevada.unr.edu>
2016-03-15 11:02:30 -0700

    Project 1 updates
    started importing enemy data into combat system

--- group_projects/js/character.js
+++ group_projects/js/character.js
@@ -3,7 +3,7 @@
     level: 1,
     currentHP: 80,
     maxHP: 100,
-    currentMana: 100,
+    currentMana: 75,
     maxMana: 100,
     currentExp: 0,
     gold: 0,
--- group_projects/js/combat.js
+++ group_projects/js/combat.js
@@ -1,8 +1,14 @@
-function beginCombat() {
+var currentEnemy
+
+function beginCombat(enemy) {
+    currentEnemy = enemy
     document.getElementsByClassName("map")[0].style.display = "none"
     document.getElementById("combatDisplay").style.display = "block"
-    updateCombatDisplay()
-    combat()
+    var node = document.createElement("Li");
+    var textnode = document.createTextNode("Entering combat with " + enemy.name);
+    node.appendChild(textnode);    
+    document.getElementById("combat-text").insertBefore(node, document.getElementById("combat-text").childNodes[0]);
+    updateCombatDisplay()    
 }
 
 function combat() {
@@ -12,6 +18,15 @@ function combat() {
 function updateCombatDisplay() {
     document.getElementById("hero-health").innerHTML = character.currentHP + " / " + character.maxHP
     document.getElementById("hero-health").style.width = (character.currentHP / character.maxHP) * 100 + "%"
+    document.getElementById("hero-mana").innerHTML = character.currentMana + " / " + character.maxMana
+    document.getElementById("hero-mana").style.width = (character.currentMana / character.maxMana) * 100 + "%"
+
+    // Update Enemy Information
+    document.getElementById("enemy-name").innerHTML = currentEnemy.name
+    document.getElementById("enemy-health").innerHTML = currentEnemy.currentHealth + " / " + currentEnemy.health
+    document.getElementById("enemy-health").style.width = (currentEnemy.currentHealth / currentEnemy.health) * 100 + "%"
+    // This needs to be fixed. The enemy level div contains the other divs, and replacing the text inside it removes them.
+    //document.getElementById("enemy-level").innerHTML = currentEnemy.level
 }
 
 function flee() {
