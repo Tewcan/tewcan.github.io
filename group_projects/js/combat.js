01bfd9525c4e7da48ef12ee372155a45d6a07e50
Mike-Tew <Mike.Tew@gmail.com>
2016-03-18 02:17:47 -0700

    Updated UI

--- group_projects/js/combat.js
+++ group_projects/js/combat.js
@@ -16,14 +16,14 @@ function combat() {
 }
 
 function updateCombatDisplay() {
-    document.getElementById("hero-health").innerHTML = character.currentHP + " / " + character.maxHP
+    document.getElementById("hero-health-number").innerHTML = character.currentHP + " / " + character.maxHP
     document.getElementById("hero-health").style.width = (character.currentHP / character.maxHP) * 100 + "%"
-    document.getElementById("hero-mana").innerHTML = character.currentMana + " / " + character.maxMana
+    document.getElementById("hero-mana-number").innerHTML = character.currentMana + " / " + character.maxMana
     document.getElementById("hero-mana").style.width = (character.currentMana / character.maxMana) * 100 + "%"
 
     // Update Enemy Information
     document.getElementById("enemy-name").innerHTML = currentEnemy.name
-    document.getElementById("enemy-health").innerHTML = currentEnemy.currentHealth + " / " + currentEnemy.health
+    document.getElementById("enemy-health-number").innerHTML = currentEnemy.currentHealth + " / " + currentEnemy.health
     document.getElementById("enemy-health").style.width = (currentEnemy.currentHealth / currentEnemy.health) * 100 + "%"
     // This needs to be fixed. The enemy level div contains the other divs, and replacing the text inside it removes them.
     //document.getElementById("enemy-level").innerHTML = currentEnemy.level
