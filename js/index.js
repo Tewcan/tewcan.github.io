1062663c9d5f6a9d1c5c9c84b968a05e6e924118
HyrumTew <lemon_bars@hotmail.com>
2016-04-25 13:32:01 -0700

    updated featured box

--- js/index.js
+++ js/index.js
@@ -4,6 +4,7 @@ images =[
 	{src:'images/loothoard2.png', href:'loothoard.html',name:"LootHoard"},
 	{src:'images/target.PNG', href:'hyrum/target.html',name:"Targets"}	]
 var imageSelector = 0
+var domNav = document.getElementsByClassName("featured__nav")
 function featuredControll(input1,input2) {
 	console.log(imageSelector)
 	if (imageSelector === 0 && input1 === -1) {}
@@ -12,6 +13,10 @@ function featuredControll(input1,input2) {
 		  changeFeatured()	}
 }
 function changeFeatured() {
+	if (imageSelector === 0) {domNav[0].style.backgroundColor = "#6b6b47"}
+	else {domNav[0].style.backgroundColor = "#ff751a"}
+	if (imageSelector === (images.length - 1)) {domNav[1].style.backgroundColor = "#6b6b47"}
+	else {domNav[1].style.backgroundColor = "#ff751a"}
 	document.getElementsByClassName("featured__title")[0].innerHTML = images[imageSelector].name
 	document.getElementsByClassName("featured__image")[0].setAttribute("src", images[imageSelector].src)
 	document.getElementsByClassName("featured__link")[0].setAttribute("href", images[imageSelector].href)
